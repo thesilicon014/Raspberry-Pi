e905cb42bc9f94bf74c1534d4c3391f0a54a2ce4
thesilicon014 <binder1@purdue.edu>
2016-11-05 18:08:23 -0400

    Update keywrite.c

--- keywrite.c
+++ keywrite.c
@@ -2,18 +2,17 @@
 #include <stdint.h>
 
 int main() {
-	uint32_t key = {
+	uint32_t key[8] = {
 		0x471b6795, 0x2e782a39, 0xbc5cc86b, 0xdb6a2302,
 		0xe213b913, 0xeaa13ff9, 0x40762ac2, 0x1eba261f };
 	
-	uint8_t count;
-	char *address = "$DEV/key.bin"; 
+	uint8_t count; 
 	FILE *key_ptr;
 	
-	key_ptr = fopen( address, "w+b");
+	key_ptr = fopen("/home/pi/development/Raspberry-Pi", "w+b");
 	
 	for(count=0; count<8; count++) {
-		fwrite(key, sizeof(key[0]), 1, key_ptr);
+		fwrite(key, sizeof(key[0]*8), 1, key_ptr);
 	}
 	
 	fclose(key_ptr);
