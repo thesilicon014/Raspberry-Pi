7c3b4d469ca8bfa58690d357d201d9a1416046ff
thesilicon014 <binder1@purdue.edu>
2016-11-05 19:31:39 -0400

    Update keywrite.c

--- keywrite.c
+++ keywrite.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <error.h>
 
-int main() {
+#define ARGCOUNT 1000
+
+extern int errno;
+
+int main( int argc, char *argv[] ) {
 	uint32_t key[8] = {
 		0x471b6795, 0x2e782a39, 0xbc5cc86b, 0xdb6a2302,
 		0xe213b913, 0xeaa13ff9, 0x40762ac2, 0x1eba261f };
 	
-	uint8_t count; 
 	FILE *key_ptr;
 	
-	key_ptr = fopen("/home/pi/development/Raspberry-Pi/key.bin", "w+b");
+	if(argc != 2) {
+		errno = ARGCOUNT;
+		perror("Command line argument error;");
+		fprintf(stderr, "Error 'argcount': %d\n\n", errno);
+		exit(EXIT_FAILURE);
+	}
+	
+	key_ptr = fopen(strcat("$KEY/", argv[1]), "w+b");
 	
 	fwrite(key, 4, 8, key_ptr);
 	
